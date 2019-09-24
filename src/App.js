5ab5ecc6607c9283ed2a6d9923d5d3b1931d323c
Carlos Junior <carlosjunior@id.uff.br>
2019-09-24 18:54:22 +0200

    [REFACT] Refact to fit in new lint rules
    It's the design standard used as reference for most of the companies.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-/* 
+/*
   Copyright 2019 Globo.com authors. All rights reserved.
 Use of this source code is governed by a BSD-style
 license that can be found in the LICENSE file.
 */
 
-import React from "react";
-import logo from "./logo.png";
-import "./App.css";
-import Dashboard from "./components/Dashboard";
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+import Dashboard from './components/Dashboard';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <div>
-          <Dashboard />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+    <div>
+      <Dashboard />
+    </div>
+  </div>
+);
 
 export default App;
