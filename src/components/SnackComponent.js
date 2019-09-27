6dd419599ba5a48f9239c04f68d08bfc33cbea54
Carlos Junior <carlosjunior@id.uff.br>
2019-09-27 14:11:58 +0200

    [STYLE] Format SnackComponent to meet lint rules.

--- src/components/SnackComponent.js
+++ src/components/SnackComponent.js
@@ -11,86 +11,106 @@ import ErrorIcon from '@material-ui/icons/Error';
 import { makeStyles } from '@material-ui/core/styles';
 
 const variantIcon = {
-	success: CheckCircleIcon,
-	error: ErrorIcon,
+  success: CheckCircleIcon,
+  error: ErrorIcon,
 };
 
-const snackstyles = makeStyles(theme => ({
-	success: {
-		backgroundColor: green[600],
-	},
-	error: {
-		backgroundColor: theme.palette.error.dark,
-	},
-	icon: {
-		fontSize: 20,
-	},
-	iconVariant: {
-		opacity: 0.9,
-		marginRight: theme.spacing(1),
-	},
-	message: {
-		fontSize: 15,
-		display: 'flex',
-		alignItems: 'center',
-	},
+const snackstyles = makeStyles((theme) => ({
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1),
+  },
+  message: {
+    fontSize: 15,
+    display: 'flex',
+    alignItems: 'center',
+  },
 }));
 
 function MySnackbarContent(props) {
-	const classes = snackstyles();
-	const { className, message, onClose, variant, ...other } = props;
-	const Icon = variantIcon[variant]
-	return (
-		<SnackbarContent
-			className={clsx(classes[variant], className)}
-			aria-describedby="client-snackbar"
-			message={
-				<span id="client-snackbar" className={classes.message}>
-					<Icon className={clsx(classes.icon, classes.iconVariant)} />
-					{message}
-				</span>
-			}
-			action={[
-				<IconButton
-					key="close"
-					aria-label="Close"
-					color="inherit"
-					className={classes.close}
-					onClick={onClose}
-				>
-					<CloseIcon className={classes.icon} />
-				</IconButton>
-			]}
-			{...other}
-		/>
-	);
+  const classes = snackstyles();
+  const {
+    className, message, onClose, variant,
+  } = props;
+  const Icon = variantIcon[variant];
+  return (
+    <SnackbarContent
+      className={clsx(classes[variant], className)}
+      aria-describedby="client-snackbar"
+      message={(
+        <span id="client-snackbar" className={classes.message}>
+          <Icon className={clsx(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+)}
+      action={[
+        <IconButton
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          className={classes.close}
+          onClick={onClose}
+        >
+          <CloseIcon className={classes.icon} />
+        </IconButton>,
+      ]}
+    />
+  );
 }
 
 MySnackbarContent.propTypes = {
-	className: PropTypes.string,
-	message: PropTypes.string,
-	onClose: PropTypes.func,
-	variant: PropTypes.oneOf(['success', 'error', '']).isRequired,
+  className: PropTypes.string,
+  message: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+  variant: PropTypes.oneOf(['success', 'error', '']).isRequired,
 };
 
-const SnackComponent = props => {
-	return (
-		<Snackbar
-			anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-			open={props.open}
-			autoHideDuration={props.duration}
-			onClose={props.onClose}
-			ContentProps={{
-				'aria-describedby': 'message-id',
-			}}
-		>
-			<MySnackbarContent
-				onClose={props.onClose}
-				variant={props.variant}
-				message={props.message}
-			/>
-		</Snackbar>
-	)
-}
+MySnackbarContent.defaultProps = {
+  className: '',
+};
+
+const SnackComponent = (props) => {
+  const {
+    open, duration, onClose, variant, message,
+  } = props;
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      open={open}
+      autoHideDuration={duration}
+      onClose={onClose}
+      ContentProps={{
+        'aria-describedby': 'message-id',
+      }}
+    >
+      <MySnackbarContent
+        onClose={onClose}
+        variant={variant}
+        message={message}
+      />
+    </Snackbar>
+  );
+};
+
+SnackComponent.propTypes = {
+  open: PropTypes.bool.isRequired,
+  duration: PropTypes.number,
+  onClose: PropTypes.func.isRequired,
+  variant: PropTypes.oneOf(['success', 'error', '']).isRequired,
+  message: PropTypes.string.isRequired,
+};
+
+SnackComponent.defaultProps = {
+  duration: null,
+};
 
 export default SnackComponent;
