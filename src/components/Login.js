7ffb37dc89f3cd63df29bf48c88fdedb96e2337b
edumoreira1506 <contato@eduardoem.com.br>
2019-10-03 13:35:22 -0300

    Added page of login

---
+++ src/components/Login.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import LoginButton from './LoginButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBitbucket, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const colorBackground = '#e6dbdb';
+const colorBackgruondBox = '#3d4572';
+
+const boxSizeWidth = 400;
+const boxSizeHeight = 260;
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div style={{ 
+        margin: 0, 
+        width: '100%', 
+        height: '100vh', 
+        background: colorBackground, 
+        position: 'relative' 
+      }}>
+        <div style={{ 
+          width: boxSizeWidth, 
+          height: boxSizeHeight, 
+          background: colorBackgruondBox, 
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%,-50%)',
+          borderRadius: '8px'
+        }}>
+          <div>
+            <h2 style={{ 
+              color: '#fff', 
+              fontSize: '25px', 
+              padding: '8px 0 10px 0' 
+            }}>Start building!</h2>
+          </div>
+          <div style={{
+            width: '100%'
+          }}>
+            <div style={{
+              display: 'block',
+              width: '100%',
+              height: '100%'
+            }}>
+              <LoginButton 
+                backgroundColorIcon='#50c94b'
+                backgroundColorText='#45b356'
+                icon={<FontAwesomeIcon icon={faGithub} />}
+                text='GitHub'
+                link='#'
+              />
+            </div>
+
+            <div style={{
+              display: 'block',
+              width: '100%',
+              height: '100%',
+              padding: '15px 0 15px 0'
+            }}>
+              <LoginButton 
+                backgroundColorIcon='#2e62d2'
+                backgroundColorText='#265dca'
+                icon={<FontAwesomeIcon icon={faBitbucket} />}
+                text='Bitbucket'
+                link='#'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
