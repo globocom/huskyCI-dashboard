6030e2817d942d2b8f54ca34c9c437406a610485
Matheus Lima <liminha@10-127-102-174.corp.globoi.com>
2019-10-09 15:10:31 -0300

    feat(login-screen): add license

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+/*
+  Copyright 2019 Globo.com authors. All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+*/
+
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
