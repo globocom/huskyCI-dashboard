4c92d7697d8b4c7bbaf1da41516c37fa395f3268
José Renato <jr.joserenato.jr@gmail.com>
2019-10-09 18:13:40 -0300

    [REFACT]: change analyses results and containers chart type to Pie

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,81 +1,82 @@
-import { Paper } from '@material-ui/core'
-import { Bar, Doughnut, Pie } from 'react-chartjs-2'
-import PropTypes from 'prop-types'
-import React from 'react'
+import { Paper } from "@material-ui/core";
+import { Bar, Doughnut, Pie } from "react-chartjs-2";
+import PropTypes from "prop-types";
+import React from "react";
 
 const barChartDefaultOptions = {
   legend: {
-    display: false
+    display: false,
   },
   scales: {
     yAxes: [
       {
         ticks: {
-          min: 0
-        }
-      }
-    ]
-  }
-}
+          min: 0,
+        },
+      },
+    ],
+  },
+};
 
 const pieChartDefaultOptions = {
   legend: {
-    display: false
-  }
-}
+    display: true,
+  },
+};
 
 const dounghnutChartDefaultOptions = {
   legend: {
-    display: false
-  }
-}
+    display: true,
+  },
+};
 
 const GraphType = {
-  Pie: 'Pie',
-  Doughnut: 'Doughnut',
-  Bar: 'Bar'
-}
-
-const Graph = props => <Paper>{renderGraph(props)}</Paper>
-
-const renderGraph = props => {
-  switch (props.type) {
-    case GraphType.Bar:
-      return asBar({ ...props })
-    case GraphType.Doughnut:
-      return asDoughnut({ ...props })
-    case GraphType.Pie:
-      return asPie({ ...props })
-    default:
-      return asBar({ ...props })
-  }
-}
+  Pie: "Pie",
+  Doughnut: "Doughnut",
+  Bar: "Bar",
+};
 
 const asDoughnut = ({
   data,
   width,
   height,
-  options = dounghnutChartDefaultOptions
-}) => <Doughnut data={data} width={width} height={height} options={options} />
+  options = dounghnutChartDefaultOptions,
+}) => <Doughnut data={data} width={width} height={height} options={options} />;
 const asPie = ({ data, width, height, options = pieChartDefaultOptions }) => (
   <Pie data={data} width={width} height={height} options={options} />
-)
+);
 const asBar = ({ data, width, height, options = barChartDefaultOptions }) => (
   <Bar data={data} width={width} height={height} options={options} />
-)
+);
+
+const renderGraph = props => {
+  switch (props.type) {
+    case GraphType.Bar:
+      return asBar({ ...props });
+    case GraphType.Doughnut:
+      return asDoughnut({ ...props });
+    case GraphType.Pie:
+      return asPie({ ...props });
+    default:
+      return asBar({ ...props });
+  }
+};
+
+const Graph = props => <Paper>{renderGraph(props)}</Paper>;
 
 Graph.defaultProps = {
   type: GraphType.Bar,
   boxMargin: 8,
   width: 400,
-  height: 300
-}
+  height: 300,
+};
 
 Graph.propTypes = {
   type: PropTypes.string,
   data: PropTypes.node.isRequired,
+  boxMargin: PropTypes.number,
   width: PropTypes.number,
-  height: PropTypes.number
-}
+  height: PropTypes.number,
+};
 
-export { Graph, GraphType }
+export { Graph, GraphType };
