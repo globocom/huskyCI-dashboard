26ef4258f1d9f789f6d01f913454605ff22392bb
Thiago Coimbra Lemos <tclemos@users.noreply.github.com>
2019-10-10 20:40:50 -0300

    Add responsive TopBar component (#34)
    * Add responsive TopBar component
    
    * Improving alt for user profile picture
    
    * Remove unnecessary comment
    
    Replace mock user info
    User menu

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ Use of this source code is governed by a BSD-style
 license that can be found in the LICENSE file.
 */
 
-import React from "react";
-import logo from "./logo.png";
-import "./App.css";
-import Dashboard from "./components/Dashboard";
+import React from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard';
+import TopBar from './components/TopBar';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-    </header>
+    <TopBar />
     <div>
       <Dashboard />
     </div>
